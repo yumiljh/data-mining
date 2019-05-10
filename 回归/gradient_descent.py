80a03ede97340cc3d28265900647e6cadb332bfd
Andy <yumilj@126.com>
2019-05-10 12:24:37 +0800

    Update gradient_descent.py

--- 回归/gradient_descent.py
+++ 回归/gradient_descent.py
@@ -2,72 +2,40 @@ import numpy as np
 
 EPSILON = 1e-5
 
-# Batch Gradient Descent
-class BGD:
+# Only for Least_squares
+# Need to rewrite for other usage
+def error_function(theta, X, y):
+	diff = np.dot(X, theta) - y
+	m = len(X)
+	return (1.0 / (2 * m)) * np.dot(np.transpose(diff), diff)
 
-	counter = 3
-
-	# Only for Least_squares
-	# Need to rewrite for other usage
-	def error_function(self, theta, X, y):
-		diff = np.dot(X, theta) - y
-		m = len(X)
-		return (1.0 / (2 * m)) * np.dot(np.transpose(diff), diff)
-
-	# Only for Least_squares
-	# Need to rewrite for other usage
-	def gradient_function(self, theta, X, y):
-		diff = np.dot(X, theta) - y
-		m = len(X)
-		return (1.0 / m) * np.dot(np.transpose(X),diff)
-
-	# theta: original theta, where to start
-	# X: training X
-	# Y: trainnig Y
-	# alpha: learning step
-	def gradient_descent(self, init_theta, X, y, alpha):
-		theta = init_theta
-		gradient = self.gradient_function(theta, X, y)
-		while not np.all(np.absolute(gradient) <= EPSILON):
-			if self.counter > 0:
-				print gradient
-				self.counter -= 1
-			theta = theta - alpha * gradient
-			gradient = self.gradient_function(theta, X, y)
-		return theta
+# Batch Gradient Descent	
+# Only for Least_squares
+# Need to rewrite for other usage
+def BGD_gradient_function(theta, X, y):
+	diff = np.dot(X, theta) - y
+	m = len(X)
+	return (1.0 / m) * np.dot(np.transpose(X),diff)
 
 # Stochastic Gradient Descent
-class SGD:
-	
-	counter = 3
+# Only for Least_squares
+# Need to rewrite for other usage
+def SGD_gradient_function(theta, X, y):
+	m = len(X)
+	i = np.random.randint(0, m)
+	diff = np.dot(X[i], theta) - y[i]
+	return (np.transpose(X[i]) * diff).reshape(-1,1)
 
-	# Only for Least_squares
-	# Need to rewrite for other usage
-	def error_function(self, theta, X, y):
-		diff = np.dot(X, theta) - y
-		m = len(X)
-		return (1.0 / (2 * m)) * np.dot(np.transpose(diff), diff)
-
-	# Only for Least_squares
-	# Need to rewrite for other usage
-	def gradient_function(self, theta, X, y):
-		m = len(X)
-		i = np.random.randint(0, m)
-		diff = np.dot(X[i], theta) - y[i]
-		return (1.0 / m) * np.dot(np.transpose(X), np.full(m, diff).reshape(-1,1))
-
-	# theta: original theta, where to start
-	# X: training X
-	# Y: trainnig Y
-	# alpha: learning step
-	def gradient_descent(self, init_theta, X, y, alpha):
-		theta = init_theta
-		gradient = self.gradient_function(theta, X, y)
-		while not np.all(np.absolute(gradient) <= EPSILON):
-			if self.counter > 0:
-				print gradient
-				self.counter -= 1
-			theta = theta - alpha * gradient
-			gradient = self.gradient_function(theta, X, y)
-		return theta
-
+# theta: original theta, where to start
+# X: training X
+# Y: trainnig Y
+# alpha: learning step
+# BGD_SGD: True for SGD, False for BGD
+def gradient_descent(init_theta, X, y, alpha, BGD_SGD = False):
+	theta = init_theta
+	gradient_function = lambda theta, X, y: SGD_gradient_function(theta, X, y) if BGD_SGD else BGD_gradient_function(theta, X, y)
+	gradient = gradient_function(theta, X, y)
+	while not np.all(np.absolute(gradient) <= EPSILON):
+		theta = theta - alpha * gradient
+		gradient = gradient_function(theta, X, y)
+	return theta
